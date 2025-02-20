3ae531ed3253f02b5df80c267f7b4e6833c842ad
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-02-20 18:13:50 +0200

    add style for StatusLabel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import ActiveLabel from './components/active-label/ActiveLabel';
+import StatusLabel, { Status } from './components/active-label/ActiveLabel';
 import './globals.css';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl text-green-400">Hello Next.js</h1>
-      <ActiveLabel>Active</ActiveLabel>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
