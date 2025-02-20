4831a75da88d58c74525b66fad29311de6cd9e01
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-02-20 17:38:11 +0200

    update tailwind 4 version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ActiveLabel from './components/active-label/ActiveLabel';
+import './globals.css';
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next.js</h1>
+      <h1 className="text-xl text-green-400">Hello Next.js</h1>
+      <ActiveLabel>Active</ActiveLabel>
     </main>
   );
 }
